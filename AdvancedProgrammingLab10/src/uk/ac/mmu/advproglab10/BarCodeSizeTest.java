6bd64aaeca0d8d01c6abac588860fbbad1534c6a
rayyanp <102375875+rayyanp@users.noreply.github.com>
2022-10-16 11:12:57 +0100

    Add files via upload

---
+++ AdvancedProgrammingLab10/src/uk/ac/mmu/advproglab10/BarCodeSizeTest.java
@@ -0,0 +1,24 @@
+package uk.ac.mmu.advproglab10;
+
+import java.io.File;
+
+import net.sourceforge.barbecue.Barcode;
+import net.sourceforge.barbecue.BarcodeFactory;
+import net.sourceforge.barbecue.BarcodeImageHandler;
+
+public class BarCodeSizeTest  {
+
+		
+	    //Get 128B Barcode instance from the Factory
+	    Barcode barcode = BarcodeFactory.createCode128B("be the coder");
+	    //barcode.setBarHeight(60);
+	   // barcode.setBarWidth(2);
+
+	    File imgFile = new File("testsize.png");
+
+	    //Write the bar code to PNG file
+	    BarcodeImageHandler.savePNG(barcode, imgFile);
+	    
+	
+
+}
